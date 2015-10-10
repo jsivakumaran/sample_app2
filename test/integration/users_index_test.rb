b7ce8da3021f4d563886e859fcbcfec715b77751
Jay Sivakumaran <jsivakumaran@outlook.com>
2015-10-09 22:22:36 -0700

    Fix bug in testing file UsersIndexTest

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,12 +6,12 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index including pagination and delete links" do
-    log_in_as(@user)
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select "div.pagination"
-    first_page_of_users = User.paginat(page: 1)
+    first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
